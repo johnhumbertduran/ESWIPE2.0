5216bafaeec6c6b37e4d8b71dd124c55863c2bf8
John Humbert Duran <johnhumbertd@gmail.com>
2022-07-01 23:50:44 +0800

    Rich Text Editor Phase 2

---
+++ ESWIPE/ESWIPE/Services/Interfaces/ISave.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+
+namespace ESWIPE.Services.Interfaces
+{
+    public interface ISave
+    {
+        //Method to save document as a file and view the saved document
+    	Task SaveAndView(string filename, string contentType, MemoryStream stream);
+    }
+
+
+}
+
--- ESWIPE/ESWIPE/Views/TitleContentViewPage.xaml.cs
+++ ESWIPE/ESWIPE/Views/TitleContentViewPage.xaml.cs
@@ -8,6 +8,12 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Syncfusion.XForms.RichTextEditor;
 
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.Reflection;
+using System.IO;
+using ESWIPE.Services.Interfaces;
+
 namespace ESWIPE.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -16,6 +22,30 @@ namespace ESWIPE.Views
         public TitleContentViewPage()
         {
             InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Method for generating RTF and load HTML to another RTE. 
+        /// </summary>
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            //"App" is the class of Portable project
+            Assembly assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream inputStream = assembly.GetManifestResourceStream(inputFilePath);
+            WordDocument document = new WordDocument(inputStream, FormatType.Docx);
+            //To-Do some manipulation
+            //To-Do some manipulation
+            //Saving the Word document
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream, FormatType.Docx);
+            stream.Position = 0;
+            //Save the stream as a file in the device and invoke it for viewing
+            Xamarin.Forms.DependencyService.Get<ISave>().SaveAndView("Result.docx", "application/msword", stream);
+            //Closes the document
+            document.Close();
+            //Please download the helper files from the below link to save the stream as file and open the file for viewing in Xamarin platform
+            //https://help.syncfusion.com/file-formats/docio/create-word-document-in-xamarin#helper-files-for-xamarin
         }
     }
 }
