5e388354880feebe9c9b28e7a72ae605203a81d4
John Humbert Duran <johnhumbertd@gmail.com>
2022-08-28 00:25:08 +0800

    Student Quiz Phase 4

--- ESWIPE/ESWIPE/ViewModels/StudentViewQuestionViewModel.cs
+++ ESWIPE/ESWIPE/ViewModels/StudentViewQuestionViewModel.cs
@@ -7,12 +7,11 @@ using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ESWIPE.ViewModels
 {
-    public class StudentViewQuizViewModel : ViewModelBase
+    public class StudentViewQuestionViewModel : ViewModelBase
     {
         #region Properties
         private bool _isRefreshing;
@@ -22,38 +21,38 @@ namespace ESWIPE.ViewModels
             set => SetProperty(ref _isRefreshing, value);
         }
 
-        private readonly IQuizService _quizService;
-        public ObservableCollection<QuizModel> Quizzes { get; set; } = new ObservableCollection<QuizModel>();
+        private readonly IQuestionService _questionService;
+        public ObservableCollection<QuestionModel> Question { get; set; } = new ObservableCollection<QuestionModel>();
         #endregion
 
         #region Constructor
-        public StudentViewQuizViewModel()
+        public StudentViewQuestionViewModel()
         {
             //Title = "Teacher's Data";
-            _quizService = DependencyService.Resolve<IQuizService>();
+            _questionService = DependencyService.Resolve<IQuestionService>();
             IsRefreshing = true;
-            GetAllQuiz();
+            GetAllQuestion();
             IsBusy = IsRefreshing = false;
         }
         #endregion
 
         #region Methods
-        private void GetAllQuiz()
+        private void GetAllQuestion()
         {
             IsBusy = true;
             Task.Run(async () =>
             {
-                var quizzesList = await _quizService.GetAllQuiz();
+                var questionsList = await _questionService.GetAllQuestion();
 
                 Device.BeginInvokeOnMainThread(() =>
                 {
 
-                    Quizzes.Clear();
-                    if (quizzesList?.Count > 0)
+                    Question.Clear();
+                    if (questionsList?.Count > 0)
                     {
-                        foreach (var quiz in quizzesList)
+                        foreach (var question in questionsList)
                         {
-                            Quizzes.Add(quiz);
+                            Question.Add(question);
                         }
                     }
                     IsBusy = IsRefreshing = false;
@@ -67,15 +66,15 @@ namespace ESWIPE.ViewModels
         public ICommand RefreshCommand => new MvvmHelpers.Commands.Command(() =>
         {
             IsRefreshing = true;
-            GetAllQuiz();
+            GetAllQuestion();
         });
 
 
-        public ICommand SelectedQuizCommand => new Xamarin.Forms.Command<QuizModel>(async (quiz) =>
+        public ICommand SelectedQuestionCommand => new Xamarin.Forms.Command<QuestionModel>(async (question) =>
         {
-            if (quiz != null)
+            if (question != null)
             {
-                    await Application.Current.MainPage.Navigation.PushAsync(new Q1StudentQuestionPage());
+                await Application.Current.MainPage.Navigation.PushAsync(new Q1StudentQuestionPage());
                 //var response = await Application.Current.MainPage.DisplayActionSheet("I would like to", "Cancel", null, "Answer");
 
                 //if (response == "Answer")
