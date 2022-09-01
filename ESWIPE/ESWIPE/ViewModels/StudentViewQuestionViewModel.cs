dec7befbbddc2e42bf2471644206a44935d592ee
John Humbert Duran <johnhumbertd@gmail.com>
2022-09-01 23:38:40 +0800

    Student Quiz Phase 8

--- ESWIPE/ESWIPE/ViewModels/StudentViewQuestionViewModel.cs
+++ ESWIPE/ESWIPE/ViewModels/StudentViewQuestionViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using ESWIPE.Models;
 using ESWIPE.Services.Interfaces;
 using ESWIPE.Views;
+using Firebase.Database;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ESWIPE.ViewModels
@@ -23,6 +27,7 @@ namespace ESWIPE.ViewModels
 
         private readonly IQuestionService _questionService;
         public ObservableCollection<QuestionModel> Question { get; set; } = new ObservableCollection<QuestionModel>();
+        public static FirebaseClient firebase = new FirebaseClient("https://eswipe-37f7c-default-rtdb.asia-southeast1.firebasedatabase.app/");
         #endregion
 
         #region Constructor
@@ -74,15 +79,62 @@ namespace ESWIPE.ViewModels
         {
             if (question != null)
             {
-                await Application.Current.MainPage.Navigation.PushAsync(new Q1StudentQuestionPage());
-                //var response = await Application.Current.MainPage.DisplayActionSheet("I would like to", "Cancel", null, "Answer");
+                var AnswerData = await GetAnswer(question.CreatedBy, question.Quarters, question.Question);
+                //await Application.Current.MainPage.Navigation.PushAsync(new Q1StudentQuestionPage());
+                //var response = await Application.Current.MainPage.DisplayActionSheet(question.Question, "Cancel", null, AnswerData.Answer, AnswerData.CorrectAnswer);
 
                 //if (response == "Answer")
                 //{
-                //    await Application.Current.MainPage.Navigation.PushAsync(new IdentificationQuizPage(quiz));
+                    await Application.Current.MainPage.Navigation.PushAsync(new IdentificationQuizPage());
                 //}
             }
         });
+
+        //Read All Answers
+        public static async Task<List<AnswerModel>> GetAllAnswers()
+        {
+            try
+            {
+                var answerlist = (await firebase
+                .Child("AnswerModel")
+                .OnceAsync<AnswerModel>()).Select(item =>
+                new AnswerModel
+                {
+                    CreatedBy = item.Object.CreatedBy,
+                    DateCreated = item.Object.DateCreated,
+                    Quarters = item.Object.Quarters,
+                    QuizType = item.Object.QuizType,
+                    QuizCode = item.Object.QuizCode,
+                    Section = item.Object.Section,
+                    Question = item.Object.Question,
+                    Answer = item.Object.Answer,
+                    CorrectAnswer = item.Object.CorrectAnswer,
+                    Key = item.Object.Key
+                }).ToList();
+                return answerlist;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error:{e}");
+                return null;
+            }
+        }
+
+        //Read Answer
+        public static async Task<AnswerModel> GetAnswer(string createdby, string quarters, string question)
+        {
+            try
+            {
+                var allAnswer = await GetAllAnswers();
+                await firebase.Child("AnswerModel").OnceAsync<AnswerModel>();
+                return allAnswer.Where(a => a.CreatedBy == createdby).Where(b => b.Quarters == quarters).Where(b => b.Question == question).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e}");
+                return null;
+            }
+        }
 
         #endregion
     }
